3cb424b0cc0a66bc10a5a43bf326cf1e390dd343
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2019-05-02 04:45:35 +0700

    component untuk Attendance

--- quasar.conf.js
+++ quasar.conf.js
@@ -37,7 +37,11 @@ module.exports = function (ctx) {
         'QList',
         'QItem',
         'QItemSection',
-        'QItemLabel'
+        'QItemLabel',
+        'QSeparator',
+        'QAvatar',
+        'QBadge',
+        'QInput'
       ],
 
       directives: [
