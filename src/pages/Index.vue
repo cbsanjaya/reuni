cc0b070f12ee84fbbeb193cbdbf0d0b0c23852f3
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2019-05-02 22:30:27 +0700

    initial dashboard page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,33 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page padding>
+    <div class="row">
+      <div class="col-md-4 col-xs-12">
+        <q-card class="my-card text-white bg-green-10 q-ma-xs">
+          <q-card-section>
+            <div class="text-h6">10000</div>
+            <div class="text-subtitle2">Total Pendapatan</div>
+          </q-card-section>
+        </q-card>
+      </div>
 
+      <div class="col-md-4 col-xs-12">
+        <q-card class="my-card text-white bg-red-10 q-ma-xs">
+          <q-card-section>
+            <div class="text-h6">50000</div>
+            <div class="text-subtitle2">Total Pengeluaran</div>
+          </q-card-section>
+        </q-card>
+      </div>
+
+      <div class="col-md-4 col-xs-12">
+        <q-card class="my-card text-white bg-blue-10 q-ma-xs">
+          <q-card-section>
+            <div class="text-h6">50000</div>
+            <div class="text-subtitle2">Saldo Akhir</div>
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
   </q-page>
 </template>
 
