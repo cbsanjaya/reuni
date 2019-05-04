215a6a23edd306ea1561a4382a1eb840fbd95b71
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2019-05-04 15:44:05 +0700

    info

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -103,7 +103,7 @@ export default {
       return {
         tooltip: {
           trigger: 'item',
-          formatter: '{a} <br/>{b} : {c} ({d}%)'
+          formatter: '{b} : {c} ({d}%)'
         },
         label: {
           formatter: '{b}: ({d}%)'
